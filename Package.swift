bd555b416bd7299935cfbb3ad606615f2155f1b3
Facephi <developer@facephi.com>
2026-07-07 19:50:59 +0200

    New SPM Version: 2.9.0

--- Package.swift
+++ Package.swift
@@ -1,26 +1,36 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "SDK-DisclaimerComponent-SPM",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SDK-DisclaimerComponent-SPM",
-            targets: ["SDK-DisclaimerComponent-SPM"]
+            targets: ["SDK-DisclaimerComponent-SPM", "disclaimerComponent"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/facephi-clienters/FPHIDisclaimerWidget-SPM.git", .exactItem("0.1.7")),
+        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.9.0")),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SDK-DisclaimerComponent-SPM"
+            name: "SDK-DisclaimerComponent-SPM",
+            dependencies: [
+                "disclaimerComponent",
+                "SDK-CorePackage-SPM",
+                .product(
+                    name: "FPHIDisclaimerWidget-SPM",
+                    package: "FPHIDisclaimerWidget-SPM"
+                ),
+            ],
         ),
-        .testTarget(
-            name: "SDK-DisclaimerComponent-SPMTests",
-            dependencies: ["SDK-DisclaimerComponent-SPM"]
+        .binaryTarget(
+            name: "disclaimerComponent",
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKDisclaimerComponent/2.9.0/disclaimerComponent.zip",
+            checksum: "93c8b3887a16c01e582cd62d7da3eba08921039eaee3d47c1f3c4a4ac5f64c2b"
         ),
     ]
 )
